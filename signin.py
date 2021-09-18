2a976dad13485aac3bd41facc575a2ce8e5678b7
wwwlight2020 <70984697+wwwlight2020@users.noreply.github.com>
2021-09-18 18:54:09 +0800

    Update signin.py

--- signin.py
+++ signin.py
@@ -3,8 +3,8 @@ import os
 import re
 
 # hao4k 账户信息
-username = os.environ["HAO4K_USERNAME"]
-password = os.environ["HAO4K_PASSWORD"]
+username = os.environ["wwwlight"]
+password = os.environ["Zhutou33"]
 # 添加 server 酱通知
 sckey = os.environ["SERVERCHAN_SCKEY"]
 send_url = "https://sctapi.ftqq.com/%s.send" % (sckey)
